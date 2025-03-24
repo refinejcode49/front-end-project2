e9779f6e55c003fc263ffe499874f425f4fa77b1
Enrique Castaneda <je.castanedam@gmail.com>
2025-03-24 14:40:48 +0100

    Co-authored-by: Jennifer  <refinejcode49@users.noreply.github.com>

--- src/pages/BooksListPage.jsx
+++ src/pages/BooksListPage.jsx
@@ -1,9 +1,8 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { data, Link } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const BooksListPage = () => {
-
   const [allBooks, setAllBooks] = useState([]);
   useEffect(() => {
     async function getAllBooks() {
@@ -11,55 +10,34 @@ const BooksListPage = () => {
         const response = await axios.get("http://localhost:5005/books");
         console.log("response: ", response);
         // in the google json file the data is stored inside items
-        setAllBooks(response.data.items)
+        setAllBooks(response.data.items);
       } catch (error) {
         console.log(error);
       }
     }
     //to work you have to call getAllbooks() !!!!!!!!!!!
     getAllBooks();
-
-    /*fetch("https://www.googleapis.com/books/v1/volumes?q=subject:fantasy&maxResults=40")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data)
-        setAllBooks(response.data.items)
-      })
-      .catch((error)=>{
-        console.log("the error is :", error);
-      })*/
-     /*axios("https://www.googleapis.com/books/v1/volumes?q=subject:fantasy&maxResults=40")
-     .then(({data}) => {
-      console.log("response from axios: ", data);
-      setAllBooks(data.items)
-     })
-     .catch((err) => {
-      console.log(err);
-     });*/
-
   }, []);
 
-
-
-
   return (
-      <div className="all-books">
-        {allBooks.map((oneBook) => {
-          return (
-            <div key={oneBook.id} className="book-card">
-              <article>
-                <Link to={"/books/:bookId"}>
-                  <img src={oneBook.volumeInfo.imageLinks.smallThumbnail} alt={oneBook.volumeInfo.description} />
-                </Link>
-                <h2>{oneBook.volumeInfo.title}</h2>
-                <p>{oneBook.volumeInfo.authors}</p>
-              </article>
-            </div>
-          );
-        })}
-      </div>
+    <div className="all-books">
+      {allBooks.map((book) => {
+        return (
+          <div key={book.id} className="book-card">
+            <article>
+              <Link to={`/books/${book.Id}`}>
+                <img
+                  src={book.volumeInfo.imageLinks.smallThumbnail}
+                  alt={book.volumeInfo.description}
+                />
+              </Link>
+              <h2>{book.volumeInfo.title}</h2>
+              <p>{book.volumeInfo.authors}</p>
+            </article>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
