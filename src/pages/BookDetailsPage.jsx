1ab9c6db3defba14e2dacf1c9b9483e8bdfa4930
Enrique Castaneda <je.castanedam@gmail.com>
2025-03-25 17:32:11 +0100

    delete button styling

--- src/pages/BookDetailsPage.jsx
+++ src/pages/BookDetailsPage.jsx
@@ -1,67 +1,59 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const BookDetailsPage = () => {
   const [book, setBook] = useState(null);
   const { bookId } = useParams();
   const navigate = useNavigate();
 
-  useEffect(()=> {
-   
+  useEffect(() => {
     fetch(`http://localhost:5005/books/${bookId}`)
-      .then((response) => {
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((data) => {
         console.log("here is one book", data);
         setBook(data);
       })
-      .catch((error)=> console.log("the error is :", error));
-    }, [bookId])
+      .catch((error) => console.log("the error is:", error));
+  }, [bookId]);
 
-    function handleDelete() {
-      // remove it from the frontend
-         //we filter the through the book, to compare if bookid is not equal to oneBook.id then we take it out
-          const filteredBook = Object.keys(book).filter(key => key.includes("id"))
-          console.log("delete button clicked ", bookId);
-        //we set setBook to the new filteredBook variable
-          setBook(filteredBook);
-      // remove it from the database with axios
-      axios
+  function handleDelete() {
+    axios
       .delete(`http://localhost:5005/books/${bookId}`)
       .then((res) => {
         console.log("book deleted", res.data);
-        setBook(filteredBook)
-        navigate("/")
-        
+        alert("Book deleted successfully!");
+        navigate("/");
       })
-      .catch((err)=> console.log(err))
-    }
+      .catch((err) => console.log(err));
+  }
 
-    if(!book) {
-      return <div>Book not found</div>
-    }
+  if (!book) {
+    return <div>Book not found</div>;
+  }
 
   return (
-    <div>
-      <article>
-         
-          <h2 className="title">Title: {book.volumeInfo?.title}</h2>
-          <p className="authors">Author: {book.volumeInfo?.authors}</p>
-          <img src={book.volumeInfo?.imageLinks?.thumbnail} alt={book.volumeInfo?.description} className="book-image" />
-          <p className="description">{book.volumeInfo?.description}</p>
-         
-      <Link to="/" className="back-btn">
-        <button>BACK</button>
-      </Link>
-      <Link className="favorite-btn">
-        <button>Favorite</button>
-      </Link>
-      <button onClick={()=> {
-        handleDelete();
-      }}>Delete book</button>
-      </article>
+    <div className="book-details-container">
+      <img
+        src={book.volumeInfo?.imageLinks?.thumbnail || "placeholder.jpg"}
+        alt={book.volumeInfo?.title || "No Title Available"}
+      />
+      <h2>{book.volumeInfo?.title || "No Title Available"}</h2>
+      <p className="authors">
+        Author: {book.volumeInfo?.authors?.join(", ") || "Unknown Author"}
+      </p>
+      <p className="description">
+        {book.volumeInfo?.description || "No description available."}
+      </p>
+      <div className="buttons">
+        <Link to="/" className="back-btn">
+          <button>Back</button>
+        </Link>
+        <button className="favorite-btn">Favorite</button>
+        <button className="delete-btn" onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
